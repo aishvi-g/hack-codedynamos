dc4a899a709fdab9ca5453205bbf9330df094d6f
aishvi-g <aishviguleria@gmail.com>
2023-03-05 07:09:54 +0530

    updates

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+import "../../styles/footer.css";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt pariatur accusamus
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Saturday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Altunizade Mah. Uskudar/İstanbul</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 555 555 55 55</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: c.eray@hotmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2023, website made by Eray Demircioglu. All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                {" "}
+                <Link to="https://www.facebook.com/eraydmrcoglu">
+                  <i class="ri-facebook-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                <Link to="https://github.com/eraydmrcoglu">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+
+              <span>
+                {" "}
+                <Link to="https://www.youtube.com/channel/UCRlE1wFdJrCXGp6bblO85ig">
+                  <i class="ri-youtube-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                {" "}
+                <Link to=" https://www.linkedin.com/in/eraydmrcoglu/">
+                  <i class="ri-linkedin-line"></i>
+                </Link>{" "}
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
